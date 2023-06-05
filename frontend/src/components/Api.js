1f702a52db2a2035265e08eb271cfde1a0e251bb
Baiturrahim Syariah <fahmi.kems15@gmail.com>
2023-06-05 15:03:24 +0700

    update to server config

--- frontend/src/components/Api.js
+++ frontend/src/components/Api.js
@@ -2,14 +2,10 @@ import axios from "axios";
 
 
 const baseUrl = () => {
-    let baseURL
-    if (process.env.NODE_ENV === 'production') {
-        baseURL = "http://192.168.100.70:3001"
-    }
-    if (process.env.NODE_ENV === 'development') {
-        baseURL = "http://localhost:3001"
-    }
-
+    
+    baseURL = "http://192.168.100.70:3001"
+    //baseURL = "http://localhost:3001"
+    
     return baseURL
 }
 
---
+++ start-transaksi-barang.js
@@ -0,0 +1,19 @@
+module.exports = {
+    apps: [
+      {
+        name: "Frontend Barang",
+        script: "index.js",
+        watch: true,
+        merge_logs: true,
+        cwd: "/var/www/transaksi-barang/frontend",
+      },
+      {
+        name: "Backend Barang",
+        script: "index.js",
+        watch: true,
+        merge_logs: true,
+        cwd: "/var/www/transaksi-barang/backend",
+      },
+      
+    ],
+  };
