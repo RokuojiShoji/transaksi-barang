9b356f146b2bbb830a506a9b10ffb648ed313fe4
root <fahmi.kems15@gmail.com>
2023-06-07 12:11:00 +0700

    add pm2 config json

--- start-transaksi-barang.js
+++ start-transaksi-barang.js
@@ -1,18 +1,25 @@
 module.exports = {
-    apps: [
-      {
-        name: "Frontend Barang",
-        script: "index.js",
-        watch: true,
-        merge_logs: true,
-        cwd: "/var/www/transaksi-barang/frontend",
+  apps: [
+    {
+     name: 'Frontend Barang',
+      script: 'serve',
+      env: {
+        PM2_SERVE_PATH: '/var/www/transaksi-barang/frontend/build',
+        PM2_SERVE_PORT: 3000,
+        PM2_SERVE_SPA: 'true'
       },
-      {
-        name: "Backend Barang",
-        script: "index.js",
-        watch: true,
-        merge_logs: true,
-        cwd: "/var/www/transaksi-barang/backend",
+      watch: true,
+      merge_logs: true
+    },
+    {
+      name: 'Backend Barang',
+      script: 'index.js',
+      watch: true,
+      merge_logs: true,
+      cwd: '/var/www/transaksi-barang/backend',
+      env: {
+        PORT: 3001
       },
-    ],
-  };
+    }
+  ]
+};
